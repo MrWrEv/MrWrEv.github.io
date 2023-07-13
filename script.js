4b83d8fe4c7c86f7d9a7de068ec42702b1c3ffe1
Mr Wright-Evans <4006154+MrWrEv@users.noreply.github.com>
2023-07-13 09:30:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,32 @@ function onDropdownChange() {
   var selectedWord = dropdown.options[dropdown.selectedIndex].value;
   var imageUrl = getImageUrl(selectedWord);
 
-  var image = document.getElementById('wordImage');
-  image.src = imageUrl;
-  image.style.display = imageUrl ? 'block' : 'none';
+  copyToClipboard(imageUrl);
+  displayMessage('Image URL copied to clipboard!');
 }
 
 function getImageUrl(word) {
   var imagePath = 'https://raw.githubusercontent.com/MrWrEv/MrWrEv.github.io/main/images/';
   var imageExtension = '.svg';
-  
+
   return imagePath + word + imageExtension;
+}
+
+function copyToClipboard(text) {
+  var textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+
+function displayMessage(message) {
+  var messageElement = document.getElementById('message');
+  messageElement.textContent = message;
+  messageElement.style.display = 'block';
+  setTimeout(function() {
+    messageElement.textContent = '';
+    messageElement.style.display = 'none';
+  }, 3000);
 }
