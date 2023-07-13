aa90c54c7a78da7312a6798aebddfbc2797a0b8c
Mr Wright-Evans <4006154+MrWrEv@users.noreply.github.com>
2023-07-13 13:52:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,29 +5,33 @@ function onDropdownChange() {
 
   var previewImage = document.getElementById('previewImage');
   var image = document.getElementById('wordImage');
-  
+
   previewImage.src = imageUrl;
   image.src = imageUrl;
 
   previewImage.style.display = imageUrl ? 'block' : 'none';
-  image.style.display =  'none';
+  image.style.display = 'none';
 }
 
 function getImageUrl(word) {
   var imagePath = 'https://raw.githubusercontent.com/MrWrEv/MrWrEv.github.io/main/images/';
   var imageExtension = '.svg';
 
-  return imagePath + word + imageExtension;
+    return imagePath + word + imageExtension;
 }
 
 function downloadImage() {
-  var image = document.getElementById('wordImage');
+  var image = document.getElementById('previewImage');
   var imageUrl = image.src;
 
-  var anchor = document.createElement('a');
-  anchor.href = imageUrl;
-  anchor.download = 'image.png';
-  anchor.click();
+  fetch(imageUrl)
+    .then(response => response.blob())
+    .then(blob => {
+      var anchor = document.createElement('a');
+      var url = URL.createObjectURL(blob);
+      anchor.href = url;
+      anchor.download = 'image.png';
+      anchor.click();
+      URL.revokeObjectURL(url);
+    });
 }
-
-
